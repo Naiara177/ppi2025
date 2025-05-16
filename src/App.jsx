1ac4a067a313aee0e252837ff9cf99a43e57c18c
Naiara177 <nayarabarrros94@gmail.com>
2025-05-16 19:02:42 +0000

    Aula 16/05/2025- criação de global e theme.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import './App.css';
+import "./syles/theme.css";
+import "./styles/global.css";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1> Meu Primeiro React App</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+        voluptatem eum ducimus aliquid quod repudiandae? Vel laborum nemo beatae
+        eveniet minus consequuntur accusamus. Inventore praesentium libero quia
+        illo, odio earum?
+      </p>
     </div>
   );
 }
-
-export default App;
