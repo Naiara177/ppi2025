593604718a4e700efad2f524d8d29a9631f46201
Naiara177 <nayarabarrros94@gmail.com>
2025-05-23 18:57:17 +0000

    Aula 23/05/2025- Props, Children.

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,23 @@ import { MyText } from "./components/MyText";
 
 export default function App() {
   return (
+    //React Fragments
     <>
-      <MyText />
-    </>
+      <MyText title= "1. Lorem  minus consequuntur">
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </MyText>
+
+       <MyText title= "2. minus consequuntur accusamus">
+       <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+        voluptatem eum ducimus aliquid quod repudiandae? Vel laborum nemo beatae
+        eveniet minus consequuntur accusamus. Inventore praesentium libero quia
+        illo, odio earum? </p>
+        </MyText>
+
+        <MyText title= "3.  Lorem ipsum dolor ">
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        voluptatem eum ducimus aliquid quod repudiandae?</p>
+        </MyText>
+        </>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,16 +1,12 @@
 import styles from "./MyText.module.css";
 
-export function MyText() {
+export function MyText({title, children}) {
   return (
     <div className={styles.container}>
     <div className={styles.div}>
-      <h1 className={styles.title}>Meu Primeiro React App</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-        voluptatem eum ducimus aliquid quod repudiandae? Vel laborum nemo beatae
-        eveniet minus consequuntur accusamus. Inventore praesentium libero quia
-        illo, odio earum?
-      </p>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.text}>{children}</p>
+      <p> </p>
     </div>
     </div>
 
