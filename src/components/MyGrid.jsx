d5da63e052ac4eb9dd48a5b6fb9bd52769346ad7
Naiara177 <nayarabarrros94@gmail.com>
2025-06-12 19:18:25 +0000

    Atividade avaliativa- parte1

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -1,38 +1,52 @@
 import styles from "./MyGrid.module.css";
 
 
-
 export function MyGrid() {
   return (
     <div className={styles.container}>
-      <header className={styles.header1} />
-      <header className={styles.header2} />
-      <aside className={styles.aside} />
-      <div className={styles.main}>
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>Card 1</h2>
-            <p>This is the first card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 2</h2>
-            <p>This is the second card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 3</h2>
-            <p>This is the third card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 4</h2>
-            <p>This is the fourth card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 5</h2>
-            <p>This is the fifth card.</p>
-          </div>
+      <header className={styles.header1}>
+        <h1>l'm just a girl </h1> 
+      </header>
+
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <img  src="https://fastly.picsum.photos/id/1067/200/300.jpg?hmac=9UpH9GvB6swkUWpIG1N53lIk9vdO4YcIwlH59M8er18"/>
+          <h2>Card 1</h2>
+          <p>Texto sobre a imagem</p>
+        </div>
+        <div className={styles.card}>
+          <img src="//fastly.picsum.photos/id/1018/200/300.jpg?hmac=IrYgAIczHOxOgmWliW3MlASD3LdAJ_aHAdh5f2aY9Sw" />
+          <h2>Card 2</h2>
+          <p>Texto sobre a imagem</p>
+           width="200" 
+           height="300"
+        </div>
+        <div className={styles.card}>
+          <img src="//fastly.picsum.photos/id/251/200/300.jpg?hmac=9xXOWzHXFkhqJDfiXSZARyy0pDmdAyazrrJw6VNgoKc" />
+          <h2>Card 3</h2>
+          <p>Texto sobre a imagem</p>
+           width="200" 
+           height="100"
         </div>
       </div>
-      <footer className={styles.footer} />
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <img src="//fastly.picsum.photos/id/69/200/300.jpg?hmac=Fi013AQLlcWNitMk_zPUagjtpMSBYGKXEfPstBEXLJA"  />
+          <h2>Card 4</h2>
+          <p>Texto sobre a imagem</p> 
+           width="200" 
+           height="300"
+        </div>
+        <div className={styles.card}>
+          <img src="https://fastly.picsum.photos/id/522/200/300.jpg?hmac=6-KFAVAX70eulRbHj_faT1bRFPGrXhPiDHXe6zPaH-4"  />
+          <h2>Card 5</h2>
+          <p>Texto sobre a imagem</p>
+        </div>
+        </div>
+      <footer className={styles.footer}>
+        <p>IFRN - Campus Macau | Técnico em Informática</p>
+        <p>Programação para Internet 2025 | Prof. Guilherme Leal Santos</p>
+      </footer>
     </div>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,14 +1,12 @@
-import styles from "./MyText.module.css";
+import styles from './MyText.module.css';
 
 export function MyText({title, children}) {
-  return (
-    <div className={styles.container}>
-    <div className={styles.div}>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.text}>{children}</p>
-      <p> </p>
-    </div>
-    </div>
-
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.div}>
+                <h1 className={styles.title}> {title} </h1>
+                {/* <p className={styles.text}> {children} </p> */}
+            </div>
+        </div>
+    );
 }
