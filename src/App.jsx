ce914950ea6011747a9ad4f03561cc2a08beccc2
Naiara177 <nayarabarrros94@gmail.com>
2025-10-25 00:09:08 +0000

    24/10 - 21:08

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { ProductList } from "./components/ProductList";
 import { Header } from "./components/Header";
-import { Route, Routes } from "react-router";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Cart } from "./components/Cart";
 import { CartProvider } from "./context/CartContext";
 import { Login } from "./components/Login";
@@ -15,14 +15,16 @@ export default function App() {
     <>
       <ToastContainer />
       <CartProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/signin" element={<Login value="signin" />} />
-          <Route path="/register" element={<Login value="register" />} />
-          <Route path="/user" element={<User />} />
-        </Routes>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/signin" element={<Login value="signin" />} />
+            <Route path="/register" element={<Login value="register" />} />
+            <Route path="/user" element={<User />} />
+          </Routes>
+        </BrowserRouter>
       </CartProvider>
     </>
   );
