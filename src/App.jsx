b2d2f9b69647cb3c4a0d57341ddf3f311ddf9d4e
Naiara177 <nayarabarrros94@gmail.com>
2025-11-17 23:21:46 +0000

    Cooomiiit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,15 @@ import { Header } from "./components/Header";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Cart } from "./components/Cart";
 import { CartProvider } from "./context/CartContext";
+<<<<<<< HEAD
 import { Login } from "./components/Login";
 import { ToastContainer } from "react-toastify";
+=======
+import { SessionProvider } from "./context/SessionContext";
+import { Login } from "./components/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+>>>>>>> fae4fa9 (Cooomiiit)
 import { User } from "./components/User";
 
 export default function App() {
@@ -14,6 +21,7 @@ export default function App() {
   return (
     <>
       <ToastContainer />
+<<<<<<< HEAD
       <CartProvider>
         <BrowserRouter>
           <Header />
@@ -26,6 +34,22 @@ export default function App() {
           </Routes>
         </BrowserRouter>
       </CartProvider>
+=======
+      <BrowserRouter>
+        <SessionProvider>
+          <CartProvider>
+            <Header />
+            <Routes>
+              <Route path="/" element={<ProductList />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/signin" element={<Login value="signin" />} />
+              <Route path="/register" element={<Login value="register" />} />
+              <Route path="/user" element={<User />} />
+            </Routes>
+          </CartProvider>
+        </SessionProvider>
+      </BrowserRouter>
+>>>>>>> fae4fa9 (Cooomiiit)
     </>
   );
 }
